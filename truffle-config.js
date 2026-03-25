6f1ade436f5fa4e200469c229f2a906e050ac8ce
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-03-25 16:01:11 +0300

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -22,7 +22,6 @@
 //
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
-
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
